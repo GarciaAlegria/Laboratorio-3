680cb8c32a5428ed29920633edfcbd505bc23216
GarciaAlegria <84537086+GarciaAlegria@users.noreply.github.com>
2022-07-24 16:07:10 -0600

    final_Lab3

--- Main.kt
+++ Main.kt
@@ -8,16 +8,16 @@ data class ItemData(
 )
 // -----------------------
 fun main() {
-    val result = processList(listOf(30, "Hola", null, null))
+    val result = processList(listOf(30, "Hola", false, null))
     println(result)
 }
 fun processList(inputList: List<Any?>?): List<ItemData>? {
     val result = inputList?.let {
         var datas = ArrayList<ItemData>()
         it.forEachIndexed{ index, list -> when (list){
-                is String -> {datas.add(ItemData(index,list,"String","L${list.length}"))}
-                is Int -> {datas.add(ItemData(index,list,"Entero", listOf(10,5,2).firstOrNull { (list % it) == 0}?.let{"M$it"}))}
-                is Boolean -> {datas.add(ItemData(index,list,"Booleano",if(list){"Verdadero"} else{"Falso"}))} else-> "Unknown"
+                is String -> {datas.add(ItemData(index,list,"string","L${list.length}"))}
+                is Int -> {datas.add(ItemData(index,list,"int", listOf(10,5,2).firstOrNull { (list % it) == 0}?.let{"It is multiple of $it"}))}
+                is Boolean -> {datas.add(ItemData(index,list,"boolean",if(list){"True"} else{"False"}))} else-> "Unknown"
                 }
             }
         return datas
